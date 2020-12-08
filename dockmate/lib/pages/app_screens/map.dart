fade042d18bd31118d4e89e36afc46afcaa9cccd
Jessica <jsscawdjj@gmail.com>
2020-12-08 03:23:50 -0500

    Show review bottom modal for map

--- dockmate/lib/pages/app_screens/map.dart
+++ dockmate/lib/pages/app_screens/map.dart
@@ -6,7 +6,8 @@ import 'package:dockmate/utils/bottombar.dart';
 import 'package:dockmate/model/listing.dart';
 
 final centre = LatLng(43.6532, -79.3832);
-final path = [];
+MapController _mapController = MapController();
+List<Placemark> _places = [];
 
 class Map extends StatefulWidget {
   String title;
@@ -26,8 +27,26 @@ class _MapState extends State<Map> {
             leading:
                 Image.asset("assets/dock.png", scale: 20, color: Colors.white),
             title: Text(widget.title),
-            actions: <Widget>[]),
+            actions: <Widget>[
+              IconButton(
+                  icon: Icon(Icons.zoom_in),
+                  onPressed: () {
+                    setState(() {
+                      _zoom += 2.0;
+                      _mapController.move(centre, _zoom);
+                    });
+                  }),
+              IconButton(
+                  icon: Icon(Icons.zoom_out),
+                  onPressed: () {
+                    setState(() {
+                      _zoom -= 2.0;
+                      _mapController.move(centre, _zoom);
+                    });
+                  })
+            ]),
         body: FlutterMap(
+          mapController: _mapController,
           options: MapOptions(
             minZoom: _zoom,
             center: centre,
@@ -37,37 +56,38 @@ class _MapState extends State<Map> {
               urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
               subdomains: ['a', 'b', 'c'],
             ),
-            MarkerLayerOptions(markers: _markers()),
+            MarkerLayerOptions(markers: _markers())
           ],
         ),
         bottomNavigationBar: BottomBar(bottomIndex: 1));
   }
 
   List<Marker> _markers() {
-    List<Marker> marks = [];
+    LatLng loc;
+    List<Marker> _marks = [];
     Listing _listing = new Listing();
 
     _listing.getAllListings().first.then((list) {
       setState(() {
-        LatLng loc;
         list.forEach((element) {
           _geolocator
               .placemarkFromAddress(element.address)
               .then((List<Placemark> places) {
             for (Placemark place in places) {
-              loc = LatLng(place.position.latitude, place.position.longitude);
-              path.add(loc);
+              _places.add(place);
             }
           });
         });
       });
     });
 
-    path.forEach((point) {
-      marks.add(Marker(
+    _places.forEach((place) {
+      loc = LatLng(place.position.latitude, place.position.longitude);
+
+      _marks.add(Marker(
           height: 45.0,
           width: 45.0,
-          point: point,
+          point: loc,
           builder: (context) => Container(
                   child: IconButton(
                 icon: Icon(
@@ -75,9 +95,37 @@ class _MapState extends State<Map> {
                   size: 25.0,
                   color: Colors.blue,
                 ),
+                onPressed: () {
+                  _showModalBottomSheet(place);
+                },
               ))));
     });
 
-    return marks;
+    return _marks;
+  }
+
+  Future<void> _showModalBottomSheet(Placemark place) {
+    String _address = place.subThoroughfare + " " + place.thoroughfare;
+    return showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 200,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(_address),
+                ElevatedButton(
+                  child: const Text('Close'),
+                  onPressed: () => Navigator.pop(context),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
